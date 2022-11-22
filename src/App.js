e3ab2c1f65185e5e9574a89df4c9e144a94e79b6
Kurmanbek Maratbekov <asanovkurmanbek93@gmail.com>
2022-11-22 15:57:01 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+    const expenses = [
+        // Data from backend
+        {
+            id: 'el1',
+            title: 'Car Insurance',
+            date: new Date(2022,1,25),
+            price: 200
+        },
+        {
+            id: 'el2',
+            title: 'Tualet paper',
+            date: new Date(2022,1,25),
+            price: 5
+        },
+        {
+            id: 'el3',
+            title: 'course',
+            date: new Date(2022,4,18),
+            price: 150
+        },
+        {
+            id: 'el4',
+            title: 'Utilites',
+            date: new Date(2022,6,7),
+            price: 100
+        }
+    ]
+  return (
+    <div className="App">
+      <ExpenseItem items={expenses[0]}/>
+      <ExpenseItem items={expenses[1]}/>
+      <ExpenseItem items={expenses[2]}/>
+      <ExpenseItem items={expenses[3]}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props) => {
+    const expenseTitle = props.items.title
+    const expenseDate = props.items.date
+    const expensePrice = props.items.price
+
+    return (
+        <div className='expense-item'>
+            <ExpenseDate expenseDate = {expenseDate}/>
+            <h2 className='expense-item__description'>{expenseTitle}</h2>
+            <div className='expense-item__price'>$ {expensePrice}</div>
+        </div>
+    );
+};
+
+export default ExpenseItem;
